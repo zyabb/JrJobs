d8da9bc744c3a09ed090e625e1e7a191d7228f9c
Ruslan Zyabbarov <rupam71@gmail.com>
2019-12-01 13:13:21 -0500

    Redis added and promisified, jobs filtering added

--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -2,6 +2,10 @@ const fetch = require('node-fetch');
 const baseURL = 'https://jobs.github.com/positions.json';
 var redis = require('redis'),
   client = redis.createClient();
+
+const { promisify } = require('util');
+const setAsync = promisify(client.set).bind(client);
+
 async function fetchGitHub() {
   let resultCount = 1,
     onPage = 0;
@@ -14,8 +18,26 @@ async function fetchGitHub() {
     console.log('got ', jobs.length, 'jobs');
     onPage++;
   }
+
+  const jrJobs = allJobs.filter(job => {
+    const jobTitle = job.title.toLowerCase();
+    //algo logic
+    if (
+      jobTitle.includes('senior') ||
+      jobTitle.includes('sr.') ||
+      jobTitle.includes('manager') ||
+      jobTitle.includes('architect')
+    ) {
+      return false;
+    }
+    return true;
+  });
   console.log(`got ${allJobs.length} jobs`);
+  console.log(`filtering out to Junior Jobs ${jrJobs.length}`);
+
+  //set in Redis
+  const success = await setAsync('github', JSON.stringify(allJobs));
+  console.log({ success });
 }
 
-fetchGitHub();
 module.exports = fetchGitHub;
