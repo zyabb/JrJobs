a763789a29475e286db89ffdccb20ffd96d7a69c
Ruslan Zyabbarov <rupam71@gmail.com>
2019-12-02 22:04:24 -0500

    Added styles to Job component. Added job location as well as time creation

--- client/src/Job.js
+++ client/src/Job.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
 const Job = ({ job }) => {
   return (
     <Paper className="job">
-      {job.title} {job.company}
+      <div className="fl">
+        <Typography variant="h6">{job.title}</Typography>
+        <Typography variant="h5">{job.company}</Typography>
+        <Typography>{job.location}</Typography>
+      </div>
+      <div>
+        <Typography>
+          {job.created_at
+            .split(' ')
+            .slice(0, 3)
+            .join(' ')}
+        </Typography>
+      </div>
     </Paper>
   );
 };
