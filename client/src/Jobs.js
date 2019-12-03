1d0dbab78cbe4964086183a59b43807785673bb9
Ruslan Zyabbarov <rupam71@gmail.com>
2019-12-02 22:42:51 -0500

    Pagination completed

--- client/src/Jobs.js
+++ client/src/Jobs.js
@@ -2,16 +2,60 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Job from './Job';
 
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+
 const Jobs = ({ jobs }) => {
+  const [activeStep, setActiveStep] = React.useState(0);
+  const jobsNum = jobs.length;
+  const numPages = Math.ceil(jobsNum / 50);
+  const jobsOnPage = jobs.slice(activeStep * 50, activeStep * 50 + 50);
+  function handleNext() {
+    setActiveStep(prevActiveStep => prevActiveStep + 1);
+  }
+
+  function handleBack() {
+    setActiveStep(prevActiveStep => prevActiveStep - 1);
+  }
+
   return (
     <div className="jobs">
       <Typography variant="h4" component="h1">
         Entry Level Software Jobs
       </Typography>
-
-      {jobs.map(job => (
-        <Job job={job} />
+      <Typography variant="h6" component="h1">
+        Found {jobsNum} Jobs
+      </Typography>
+      {jobsOnPage.map((job, i) => (
+        <Job key={i} job={job} />
       ))}
+      <div>
+        Page {activeStep + 1} of {numPages}
+      </div>
+      <MobileStepper
+        variant="progress"
+        steps={numPages}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button
+            size="small"
+            onClick={handleNext}
+            disabled={activeStep === numPages - 1}
+          >
+            Next
+            <KeyboardArrowRight />
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            <KeyboardArrowLeft />
+            Back
+          </Button>
+        }
+      />
     </div>
   );
 };
